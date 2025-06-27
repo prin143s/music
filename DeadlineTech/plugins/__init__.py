7932a1e813480f74c7329487cc1d6dcf9c698a9b
prin143s <princekgupta0000000000@gmail.com>
2025-06-27 15:12:17 +0530

    Update __init__.py

--- DeadlineTech/plugins/__init__.py
+++ DeadlineTech/plugins/__init__.py
@@ -16,4 +16,21 @@ def __list_all_modules():
 
 
 ALL_MODULES = sorted(__list_all_modules())
+__all__ = ALL_MODULES + ["import glob
+from os.path import dirname, isfile
+
+def __list_all_modules():
+    work_dir = dirname(__file__)
+    mod_paths = glob.glob(work_dir + "/*/*.py")
+    all_modules = [
+        ((f.replace(work_dir + "/", "").replace("/", "."))[:-3])
+        for f in mod_paths
+        if isfile(f) and not f.endswith("__init__.py")
+    ]
+    return all_modules
+
+ALL_MODULES = sorted(__list_all_modules())
 __all__ = ALL_MODULES + ["ALL_MODULES"]
+
+print("[+] Loaded modules:", ALL_MODULES)  # Debugging line
+"]
